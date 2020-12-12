477d8f3b25d744ffa94a51961b035d3991a80f16
sangyetenphel <sangyetenphel@gmail.com>
2020-12-12 13:10:24 -0500

    Added description to product model and sign in to review option

--- store/models.py
+++ store/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.name
