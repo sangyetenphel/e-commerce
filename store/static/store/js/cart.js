9b17b823a96dab50def30ef93772815d766afa1c
sangyetenphel <sangyetenphel@gmail.com>
2020-12-21 17:25:09 -0500

    Temp fix

--- store/static/store/js/cart.js
+++ store/static/store/js/cart.js
@@ -4,9 +4,9 @@ for(var i=0; i<updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
         var productID = this.dataset.product
         var action = this.dataset.action
-        console.log('productID:', productID, 'action:', action)
+        // console.log('productID:', productID, 'action:', action)
         
-        console.log('USER:', user)
+        // console.log('USER:', user)
         if(user === 'AnonymousUser'){
             addCookieItem(productID, action)
         }else{
